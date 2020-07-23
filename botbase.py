6f41006c1c6bb04ed3a2844a3f023f394d5a66f9
Seyona <dgbaker93@gmail.com>
2020-07-23 08:44:52 -0500

    load class data, and add all emojis

--- botbase.py
+++ botbase.py
@@ -6,6 +6,7 @@ import os
 import sys
 #End Discord requirements
 
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,36 +15,44 @@ GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
 
+discordEmojis = []
+
+with open('classes.json') as json_file:
+	classData = json.load(json_file)
+	classEmojisNames = classData.keys()
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord')
-    
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+	print(f'{client.user} has connected to Discord')
+	
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
 
-    for channel in guild.text_channels:
-        if channel == "botfuckery":
-            break
-    
-    for emoji in guild.emojis:
-        if emoji.name == 'seyonirl':
-            break
+	for channel in guild.text_channels:
+		if channel == "botfuckery":
+			break
 
-    await channel.purge()
-    await channel.send("bitches")
-    msg = channel.last_message
-    await msg.add_reaction(emoji)
+	await channel.purge()
+	await channel.send("Pick yo class fool")
+	msg = channel.last_message
+
+	for emoji in guild.emojis:
+		if emoji.name in classEmojisNames:
+			discordEmojis.append(emoji)
+			msg.add_reaction(emoji)
+			if discordEmojis.count == 8: # 8 classes no more lookup required
+				break
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    #add some checks to what channel it is in....
+	if message.author == client.user:
+		return
+	#add some checks to what channel it is in....
 
-    user = await client.fetch_user(message.author.id)
-    await message.channel.send(f'Hi {user}')
+	user = await client.fetch_user(message.author.id)
+	await message.channel.send(f'Hi {user}')
 
 
 client.run(TOKEN)
