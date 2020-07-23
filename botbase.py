5ad4244ab57b6a331d4a8b169f61fcdfbab89982
Seyona <dgbaker93@gmail.com>
2020-07-23 13:05:47 -0500

    removing on_message function, and change up msg id fetching

--- botbase.py
+++ botbase.py
@@ -16,6 +16,7 @@ GUILD = os.getenv("DISCORD_GUILD")
 client = discord.Client()
 
 discordEmojis = []
+msgId = 0
 
 with open('classes.json') as json_file:
 	classData = json.load(json_file)
@@ -34,25 +35,14 @@ async def on_ready():
 			break
 
 	await channel.purge()
-	await channel.send("Pick yo class fool")
-	msg = channel.last_message
+	sent = await channel.send("Pick yo class fool")
+	msgId = sent.id
 
 	for emoji in guild.emojis:
 		if emoji.name in classEmojisNames:
 			discordEmojis.append(emoji)
-			await msg.add_reaction(emoji)
+			await msgId.add_reaction(emoji)
 			if discordEmojis.count == 8: # 8 classes no more lookup required
 				break
-
-
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-	#add some checks to what channel it is in....
-
-	user = await client.fetch_user(message.author.id)
-	await message.channel.send(f'Hi {user}')
-
 
 client.run(TOKEN)
