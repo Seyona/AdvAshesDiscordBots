bf1910692f075d80b31ff87ad0634a02eec43cfa
Seyona <dgbaker93@gmail.com>
2020-07-25 15:11:57 -0500

    move role adds to functions to clean up reaction function

--- botbase.py
+++ botbase.py
@@ -19,7 +19,8 @@ GUILD = os.getenv("DISCORD_GUILD")
 client = discord.Client()
 
 discordEmojis = []
-discordRoles = []
+primaryClassRoles = []
+augmentClassRoles = []
 primaryClassMsgId = 0
 secondaryClassMsgId = 0
 isReady = False
@@ -27,7 +28,9 @@ isReady = False
 with open('classes.json') as json_file:
 	classData = json.load(json_file)
 	classNames = classData.keys()
+	augmentNames = [item for innerList in classData.values() for item in innerList.values()]
 
+# On ready prep function
 @client.event
 async def on_ready():
 	#global calls so we can modify these variables
@@ -35,7 +38,8 @@ async def on_ready():
 	global secondaryClassMsgId
 	global discordEmojis
 	global isReady
-	global discordRoles
+	global primaryClassRoles
+	global augmentClassRoles
 
 	print(f'{client.user} has connected to Discord')
 	
@@ -63,11 +67,14 @@ async def on_ready():
 
 	for role in guild.roles:
 		if role.name in classNames:
-			discordRoles.append(role)
+			primaryClassRoles.append(role)
+		if role.name in augmentNames:
+			augmentClassRoles.append(role)
 
 	isReady = True
 	print("Setup complete")
 
+# How the bot will handle reactions
 @client.event
 async def on_reaction_add(reaction,user):
 	if not isReady or user.id == 735708593294147674:
@@ -83,24 +90,18 @@ async def on_reaction_add(reaction,user):
 		return
 
 	# get requested role
-	for role in discordRoles:
+	for role in primaryClassRoles:
 		if role.name == reaction.emoji.name:
 			requestedRoleName = reaction.emoji.name
 			roleSelectionString = f'<@{user.id}> You selected the {requestedRoleName}'
 			requestedRole = role
 			break
-
-	if reactMsgId == primaryClassMsgId:
-		# remove existing role
-		for role in user.roles:
-			if role.name in classNames:
-				await user.remove_roles(role)
-
-		roleConfirmMsg = await reaction.message.channel.send(f'{roleSelectionString} as your primary role. Don\'t forget to select an augmenting class!')
-		await user.add_roles(requestedRole)
-
-		await DeleteMessageFromReaction(reaction,roleConfirmMsg, 5)
-
+	
+	#First Message was clicked, assign the user a role and move on
+	if reactMsgId == primaryClassMsgId: 
+		await SetPrimaryClassRole(user,reaction, classNames, requestedRoleName, roleSelectionString)
+		
+	#second message was clicked, make sure they have a primary class role assigned, assign an augment class role
 	elif reactMsgId == secondaryClassMsgId:
 		#Get current role
 		for role in user.roles:
@@ -115,15 +116,47 @@ async def on_reaction_add(reaction,user):
 			await DeleteMessageFromReaction(reaction, pickFromtheFirstMsg, 5)
 
 		else:
-			selectedCombo = classData[currentRoleName][requestedRoleName]
-			comboMsg = await reaction.message.channel.send(f'{roleSelectionString} as your augment role. You are a {selectedCombo.capitalize()}! <a:Wow:694922547812368445>')
-			await  DeleteMessageFromReaction(reaction, comboMsg, 5)#might be able to remove sleep if google sheets take awhile
+			await SetAugmentingClassRole(user, reaction, currentRoleName, requestedRoleName, augmentClassRoles, roleSelectionString)
 		
-		# Gotta do google spreadsheet magic here....yeepie	
+			# Gotta do google spreadsheet magic here....yeepie	
 	await reaction.message.remove_reaction(reaction.emoji, user) #clean up
 
+
+# Set the primary role of a given user based on the passed reaction
+async def SetPrimaryClassRole(user, reaction, classNames, requestedRole, roleSelectionString):
+	# remove existing role
+	RemoveRole(user, classNames)
+
+	roleConfirmMsg = await reaction.message.channel.send(f'{roleSelectionString} as your primary role. Don\'t forget to select an augmenting class!')
+	await user.add_roles(requestedRole)
+
+	await DeleteMessageFromReaction(reaction,roleConfirmMsg, 5)
+
+
+#Set the augmented class role for a given user based on the passed reaction
+async def SetAugmentingClassRole(user, reaction, currentRole, requestedRole, augmentClassRoles, roleSelectionString):
+	selectedCombo = classData[currentRole][requestedRole]
+	RemoveRole(user, augmentNames) #remove augment class, if it exists
+	#get augmenting role
+	for role in augmentClassRoles:
+		if role.name == selectedCombo:
+			augmentClassRole = role
+			break
+
+	comboMsg = await reaction.message.channel.send(f'{roleSelectionString} as your augment role. You are a {selectedCombo.capitalize()}! <a:Wow:694922547812368445>')
+	await user.add_roles(augmentClassRole)
+	await  DeleteMessageFromReaction(reaction, comboMsg, 5)
+
+# Delete the message from the given reaction's channel
 async def DeleteMessageFromReaction(reaction, msg, sleepTime=0):
 	time.sleep(sleepTime)
 	await reaction.message.channel.delete_messages([msg])
+
+# Remove a user's role if it is in the passed roles list
+async def RemoveRole(user, rolesList):
+	for role in user.roles:
+			if role.name in rolesList:
+				await user.remove_roles(role)
+				return
 
 client.run(TOKEN)
