9d302794b46896599b9c86d0a5dd70dfd02106cd
Seyona <dgbaker93@gmail.com>
2020-07-25 20:00:23 -0500

    format testing

--- botbase.py
+++ botbase.py
@@ -47,13 +47,13 @@ with open('discordIds.json') as json_file:
 summStr = f'❖ Summoner {discordIds["summoner"]}'
 longestStr = len('❖ Summoner ')
 
-bardStr    = f'❖ Bard {discordIds["bard"]}'
-clericStr  = f'❖ Cleric {discordIds["cleric"]}'
-fighterStr = f'❖ Fighter {discordIds["fighter"]}'
-mageStr    = f'❖ Mage {discordIds["mage"]}'
-rangerStr  = f'❖ Ranger {discordIds["ranger"]}'
-rogueStr   = f'❖ Rouge {discordIds["rogue"]}'
-tankStr    = f'❖ Tank {discordIds["tank"]}'
+bardStr    = f'❖ Bard            {discordIds["bard"]}'
+clericStr  = f'❖ Cleric          {discordIds["cleric"]}'
+fighterStr = f'❖ Fighter         {discordIds["fighter"]}'
+mageStr    = f'❖ Mage            {discordIds["mage"]}'
+rangerStr  = f'❖ Ranger          {discordIds["ranger"]}'
+rogueStr   = f'❖ Rouge           {discordIds["rogue"]}'
+tankStr    = f'❖ Tank            {discordIds["tank"]}'
 
 
 # On ready prep function
