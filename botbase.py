aa41ff131c2e4f893c90218bf9594a8a78cfa553
Seyona <dgbaker93@gmail.com>
2020-07-26 02:17:31 -0500

    add lifestyle emojis to whitelist

--- botbase.py
+++ botbase.py
@@ -125,6 +125,7 @@ async def on_ready():
 		if role.name in augmentNames:
 			augmentClassRoles.append(role)
 
+	discordEmojis.append(discordIds["pve"],discordIds["pvp"],discordIds["lifeskiller"])
 	await gather(playStyleMsg.add_reaction(discordIds["pve"]),playStyleMsg.add_reaction(discordIds["pvp"]),playStyleMsg.add_reaction(discordIds["lifeskiller"]))
 
 	isReady = True
