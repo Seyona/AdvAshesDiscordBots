16ebed2fe22236e0dd2e5c4569fb52166f4b544f
Seyona <dgbaker93@gmail.com>
2020-11-29 16:18:33 -0600

    Add new strings

--- helpers.py
+++ helpers.py
@@ -1,1 +1,4 @@
 baseDir = "/discordBot/AdvAshesDiscordBots/"
+eventFormatMessage = "!create Event Name, Event Description, mm/dd hhmm (military time), reccuring (yes/no), " \
+                     "days between recurrences"
+createEventEx = "!create Our Cool Event, We do Cool things, 01/01 1345, yes, 7"
