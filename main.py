2411c4d9ca68087359f3a93b5a5fc55eb5be1a59
root <dgbaker93@gmail.com>
2021-01-14 18:09:43 +0000

    pass the reaction, not hte message attatched to said reaction

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ async def on_reaction_add(reaction, user):
     chanId = reaction.message.channel.id
 
     if chanId == chanIds["roster"]:
-        await RolesManager.ReactionAdded(reaction.message, user)
+        await RolesManager.ReactionAdded(reaction, user)
 
     elif chanId == chanIds["events"]:
         return
