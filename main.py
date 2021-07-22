d07f15e9489c065e2f813c3b01314c7d2ba76e1a
Seyona <dgbaker93@gmail.com>
2021-07-22 14:41:17 -0500

    remove extra space

--- main.py
+++ main.py
@@ -339,7 +339,7 @@ async def on_message(message):
                     co_lead = order[2]
 
                 out_message = (f'** {order[0].capitalize()} ** \n'
-                            f'```asciidoc \n'
+                            f'```asciidoc\n'
                             f'[Captain]\n'
                             f'{order[1]}\n'
                             f'[Knight]\n'
